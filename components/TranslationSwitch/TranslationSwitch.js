8b0e49357eb405441d3d3c12c5706a76b5054f6c
grischad <67879713+grischad@users.noreply.github.com>
2023-07-19 13:41:41 +0200

    fix: class type

--- components/TranslationSwitch/TranslationSwitch.js
+++ components/TranslationSwitch/TranslationSwitch.js
@@ -26,7 +26,7 @@ export default function TranslationSwitch({ translations }) {
     return (
         <div className="flex items-center space-x-2">
             <label
-                class="inline-block pl-[0.15rem] hover:cursor-pointer"
+                className="inline-block pl-[0.15rem] hover:cursor-pointer"
 
             >DE</label
             >
@@ -38,21 +38,11 @@ export default function TranslationSwitch({ translations }) {
                 checked={currentLanguage === 'en'}
                 onChange={toggleLanguage} />
             <label
-                class="inline-block pl-[0.15rem] hover:cursor-pointer"
+                className="inline-block pl-[0.15rem] hover:cursor-pointer"
 
             >EN</label
             >
 
-            {/* <label class="relative flex justify-between items-center group p-2 text-xl">
-                EN
-                <input type="checkbox"
-                    role="switch"
-                    id="flexSwitchCheckDefault"
-                    checked={currentLanguage === 'EN'}
-                    onChange={toggleLanguage} class="absolute left-1/2 -translate-x-1/2 w-full h-full peer appearance-none rounded-md" />
-                <span class="w-16 h-10 flex items-center flex-shrink-0 ml-4 p-1 bg-gray-300 rounded-full duration-300 ease-in-out peer-checked:bg-green-400 after:w-8 after:h-8 after:bg-white after:rounded-full after:shadow-md after:duration-300 peer-checked:after:translate-x-6 group-hover:after:translate-x-1"></span>
-                DE
-            </label> */}
 
 
         </div>
