1e4b3a75e21bb3853972572fc0c95bf4d6340ca5
grischad <67879713+grischad@users.noreply.github.com>
2023-07-20 13:54:49 +0200

    feat: zurück zu vorher

--- pages/_app.js
+++ pages/_app.js
@@ -3,21 +3,17 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import { FaustProvider } from '@faustwp/core';
 import '@faustwp/core/dist/css/toolbar.css';
-import { WordPressBlocksProvider } from '@faustwp/blocks';
-import blocks from '../wp-blocks';
 import '../styles/global.scss';
+import '../globalStylesheet.css';
 
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
   return (
     <FaustProvider pageProps={pageProps}>
-      <WordPressBlocksProvider
-        config={{
-          blocks,
-        }}>
-        <Component {...pageProps} key={router.asPath} />
-      </WordPressBlocksProvider>
+
+      <Component {...pageProps} key={router.asPath} />
+
     </FaustProvider>
   );
 }
