b9a933cf799ef3eb9cd2ec903b105b6062bb217f
grischad <67879713+grischad@users.noreply.github.com>
2023-09-27 11:42:48 +0200

    feat: startseite + Schriftgrößen

--- wp-templates/front-page.js
+++ wp-templates/front-page.js
@@ -1,83 +1,17 @@
-import { useQuery, gql } from '@apollo/client';
-import * as MENUS from '../constants/menus';
-import { BlogInfoFragment } from '../fragments/GeneralSettings';
-import {
-  Header,
-  Footer,
-  Main,
-  Container,
-  NavigationMenu,
-  Hero,
-  SEO,
-} from '../components';
-import Image from 'next/image'
+import { useEffect } from 'react';
 
 export default function Component() {
-  const { data } = useQuery(Component.query, {
-    variables: Component.variables(),
-  });
 
-  const { title: siteTitle, description: siteDescription } =
-    data?.generalSettings;
-  const primaryMenu = data?.headerMenuItems?.nodes ?? [];
-  const footerMenu = data?.footerMenuItems?.nodes ?? [];
 
-  return (
-    <>
-      <SEO title={siteTitle} description={siteDescription} />
-      {/* <Header
-        title={siteTitle}
-        description={siteDescription}
-        menuItems={primaryMenu}
-      /> */}
-      <Main>
-        <Container>
-          <Hero title={siteTitle} />
-          <div className="text-center">
-            <p>{siteDescription}</p>
-            <p>Seite im Aufbau</p>
-            <div className="flex justify-center mb-40">
-              <Image
-                src="/logo.png" // Pfad zu Ihrem Logo
-                alt="Beschreibung des Bildes"
-                width={174} // gewünschte Breite
-                height={103} // gewünschte Höhe
-              />
-            </div>
-          </div>
-        </Container>
-      </Main>
-      <Footer title={siteTitle} menuItems={footerMenu} />
-    </>
-  );
+
+
+  useEffect(() => {
+    const newPath = `/de/startseite`;
+    window.location.href = newPath;
+
+  }, []);
+
+
+
+  return <></>
 }
-
-Component.query = gql`
-  ${BlogInfoFragment}
-  ${NavigationMenu.fragments.entry}
-  query GetPageData(
-    $headerLocation: MenuLocationEnum
-    $footerLocation: MenuLocationEnum
-  ) {
-    generalSettings {
-      ...BlogInfoFragment
-    }
-    headerMenuItems: menuItems(where: { location: $headerLocation }) {
-      nodes {
-        ...NavigationMenuItemFragment
-      }
-    }
-    footerMenuItems: menuItems(where: { location: $footerLocation }) {
-      nodes {
-        ...NavigationMenuItemFragment
-      }
-    }
-  }
-`;
-
-Component.variables = () => {
-  return {
-    headerLocation: MENUS.PRIMARY_LOCATION,
-    footerLocation: MENUS.FOOTER_LOCATION,
-  };
-};
